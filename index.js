38621cbb511ff1c483a25a102c85b5ed4e0c95fa
Tejaswini306 <saitejaswinij2002@gmail.com>
2023-06-22 21:25:06 +0530

    update

--- index.js
+++ index.js
@@ -3,20 +3,20 @@ const app = express()
 const port=5000;
 const mongoDB=require('./db')
 mongoDB();
-/*app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "http://13.51.196.32:3000");
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "http://16.171.44.105:3000");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept"
   );
   next();
-});*/
-app.use((req, res, next) => {
+});
+/*app.use((req, res, next) => {
   const allowedOrigin = `${req.protocol}://${req.get('host')}`;
   res.setHeader('Access-Control-Allow-Origin', allowedOrigin);
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   next();
-});
+});*/
 
 app.use(express.json())
 app.use('/api/auth',require("./Routes/Author"));
